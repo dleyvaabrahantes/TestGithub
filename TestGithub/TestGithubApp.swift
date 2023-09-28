d8468834b77a2cbbbd2bdb49b50cb21bbbe9438a
David Leyva <dleyvaabrahantes@gmail.com>
2023-09-28 09:58:39 -0400

    Initial Commit

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by David on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
